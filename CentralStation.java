e8c7bc534ea233fa20cb485fc6339db1e21251e1
shayorshay <afua.grundmann@gmail.com>
2017-03-22 15:56:26 +0100

    Add files via upload

--- CentralStation.java
+++ CentralStation.java
@@ -7,13 +7,10 @@ public class CentralStation extends Subject{
 	BufferedImage[] pictureArray;
 	Environment mainEnvironment;
 	RoverFactory roverFactory;
-	String typeOfRover;
-	State state;
 	
 	
 	CentralStation() {
-		typeOfRover = "CAMERAROVER";
-		state = State.STILL;
+		
 	}
 
 	
@@ -26,12 +23,17 @@ public class CentralStation extends Subject{
 		Environment cell3 = new Environment(lengthOfEachCell, widthOfEachCell);
 		Environment cell4 = new Environment(lengthOfEachCell, widthOfEachCell);
 		
-		Rover rover1 = roverFactory.getRoverType(typeOfRover, cell1);
-		Rover rover2 = roverFactory.getRoverType(typeOfRover, cell2);
-		Rover rover3 = roverFactory.getRoverType(typeOfRover, cell3);
-		Rover rover4 = roverFactory.getRoverType(typeOfRover, cell4);
+		Rover rover1 = roverFactory.getRoverType("rover1", cell1);
+		Rover rover2 = roverFactory.getRoverType("rover2", cell2);
+		Rover rover3 = roverFactory.getRoverType("rover3", cell3);
+		Rover rover4 = roverFactory.getRoverType("rover4", cell4);
 		
 		//put rovers in an array as observers.
+		
+		attach(rover1);
+		attach(rover2);
+		attach(rover3);
+		attach(rover4);
 	}
 	
 	
