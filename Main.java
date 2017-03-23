a3d6db45b00b11ff62750d598aeacd91fd40a64e
shayorshay <afua.grundmann@gmail.com>
2017-03-23 13:04:21 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,28 @@
 package rovuSystem;
 
+
 import simbad.gui.*;
 import simbad.sim.*;
 import javax.vecmath.Vector3d;
 
+
+/**
+  Derivate your own code from this example.
+ */
+
+
 public class Main {
-	
-	
+
 	public static void main(String[] args) {
-		
-		
-	// request antialising so that diagonal lines are not "stairy"
-        System.setProperty("j3d.implicitAntialiasing", "true");
-        
-        // creation of the environment containing all obstacles and robots
-        EnvironmentDescription environment = new EnvironmentSimulator();
-		
-	// create a single instance of central station and let it do its thing
-	CentralStation centralStation = CentralStation.getInstance();
-	
-	// here we create an instance of the whole Simbad simulator and we assign the newly created environment 
-        Simbad frame = new Simbad(environment, false);
-        frame.update(frame.getGraphics());
-		
+
+		System.setProperty("j3d.implicitAntialiasing", "true");
+
+		EnvironmentDescription environment = new EnvironmentSimulator();
+
+		CentralStation centralStation = CentralStation.getInstance();
+
+		Simbad frame = new Simbad(environment, false);
+		frame.update(frame.getGraphics());
 	}
 
-}
+} 
