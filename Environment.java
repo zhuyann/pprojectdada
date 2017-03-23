bcc9e154293525270cdf0aaf5e2917191ce5a119
tmv200 <t.marjanov@student.vu.nl>
2017-03-23 16:53:46 +0100

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -1,57 +1,63 @@
 package rovuSystem;
 
 public class Environment {
-	
+
 	int length;
 	int width;
-	int numberOfCoordinatesDefined;
-	int lengthOfCoordinatePool;
-	Coordinate[] coordinatePool;
+	int numberOfCoordinatesDefined;      // = all
+	int lengthOfCoordinatePool;			// = unvisited
 	Coordinate startingPoint;
-	
-	
+	Coordinate[] coordinatePool;
+
+
 	Environment() {
-		length = 0;
-		width = 0;
-		lengthOfCoordinatePool = 0;
-		numberOfCoordinatesDefined = 0;
+		length = 4;
+		width = 3;
+		startingPoint = new Coordinate(-4,0);
+
+		numberOfCoordinatesDefined = (length-1)*(width-1);
+		lengthOfCoordinatePool = numberOfCoordinatesDefined;
 		coordinatePool = new Coordinate[lengthOfCoordinatePool];
 	}
-	
+
 	Environment(int length, int width) {
 		this.length = length;
 		this.width = width;
 	}
-	
+
 	Environment(int length, int width, Coordinate startingPoint) {
 		this.length = length;
 		this.width = width;
 		this.startingPoint = startingPoint;
 	}
-	
-	void defineCoordinates(Coordinate coordinate) {
-		coordinatePool[lengthOfCoordinatePool] = coordinate;
-		lengthOfCoordinatePool++;
-		numberOfCoordinatesDefined++;
+
+	void defineCoordinates() {
+		int counter = 0;
+		for (int i = startingPoint.xValue+1, x = 1 ; x < length; i ++, x++) {
+			for (int j = startingPoint.yValue+1, y = 1; y < width; j ++, y++) {
+				coordinatePool[counter] = new Coordinate(i, j);
+				counter++;
+			}
+		}
 	}
 
 	void removeFromCoordinatePool(Coordinate coordinate) {	//new method
 		for (int i=0; i<lengthOfCoordinatePool; i++) {
 			if (coordinatePool[i] == coordinate) {
 				for (int j = i+1; j<lengthOfCoordinatePool; j++) {
-				coordinatePool[i] = coordinatePool[j];
-				i++;
+					coordinatePool[i] = coordinatePool[j];
+					i++;
 				}
 				lengthOfCoordinatePool--;
 			} 
 		}
 	}
-	
-	
+
+
 	double percentageVisited() {
 		double percentage = (numberOfCoordinatesDefined-lengthOfCoordinatePool)/numberOfCoordinatesDefined * 100;
 		return percentage;
 	}
-	
-	
+
+
 }
