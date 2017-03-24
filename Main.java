3a71e9f52471b390dc65d7783480e957bc8b54e0
tmv200 <t.marjanov@student.vu.nl>
2017-03-24 17:26:48 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,12 +18,14 @@ public class Main {
 		System.setProperty("j3d.implicitAntialiasing", "true");
 
 		EnvironmentSimulator environment = new EnvironmentSimulator();
+		
 		CentralStation centralStation = CentralStation.getInstance(environment);
 		centralStation.intializeMission();
 		
 		Simbad frame = new Simbad(environment, false);
-		frame.update(frame.getGraphics());
 		
+		frame.update(frame.getGraphics());
+		centralStation.runMission();
 	}
 
 } 
