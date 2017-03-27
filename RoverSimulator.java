cbb29e0faf1db21f6b6b4d8ade28875b18db51dc
tmv200 <t.marjanov@student.vu.nl>
2017-03-27 20:49:13 +0200

    Update RoverSimulator.java

--- RoverSimulator.java
+++ RoverSimulator.java
@@ -10,9 +10,8 @@ import simbad.sim.CameraSensor;
 import simbad.sim.RobotFactory;
 
 public class RoverSimulator extends Agent {
-	private String currentMode;
-	private int maxDistance = 4;                   // hardcoded in this case, we should calculate it instead
-	//private int maxDistance = EnvironmentSimulator.WORLD_SIZE / 2 - 1;	
+	private State state;
+
 	double Velocity;
 	Vector3d initialPosition;
 
@@ -20,18 +19,26 @@ public class RoverSimulator extends Agent {
 	int numberOfImages;
 	BufferedImage cameraImage;
 	CameraSensor camera;
-	Environment myCell;
 
-	public RoverSimulator (Vector3d position, String name, Environment cell){   
+	Environment myCell;
+	Environment mainEnvironment;
+	private int maxDistance;
+
+	public RoverSimulator (Vector3d position, String name, Environment cell, Environment environment){   
 		super(position,name);
+		RobotFactory.addSonarBeltSensor(this, 4);
+
 		Velocity = 0.5;
 		initialPosition = position;
-		myCell = cell;
-		RobotFactory.addSonarBeltSensor(this, 4);  
-		camera = RobotFactory.addCameraSensor(this);
-		cameraImage = camera.createCompatibleImage();
+
 		picArray = new BufferedImage[10];
 		numberOfImages = 0;
+		camera = RobotFactory.addCameraSensor(this);
+		cameraImage = camera.createCompatibleImage();
+
+		myCell = cell;
+		mainEnvironment = environment; 
+		maxDistance = myCell.length-1;
 	}
 
 	private Point3d location() {
@@ -40,68 +47,51 @@ public class RoverSimulator extends Agent {
 		return loc;
 	}
 
-
 	public void initBehavior() {
-		setTranslationalVelocity(10.5);
+		setTranslationalVelocity(1);
 	}
 
 	private boolean onTheCellEdge() {
-
 		Point3d loc = location();
 
-
 		return initialPosition.getX()-1 >= loc.getX() || loc.getX() >=(initialPosition.getX() + maxDistance)
-				|| initialPosition.getZ()+1 <= loc.getZ() || loc.getZ() <=(initialPosition.getZ() - maxDistance);
-
+			|| initialPosition.getZ()+1 <= loc.getZ() || loc.getZ() <=(initialPosition.getZ() - maxDistance);
 	}
 
-	public void performBehavior() {
-
-
+	public void performBehavior() {	
 		if (onTheCellEdge()) {
-			this.currentMode = "onTheCellEdge";
+			this.state = State.EDGE_OF_CELL;
 		} else if (this.collisionDetected()) {
-			this.currentMode = "avoidObstacle";	
+			this.state = State.AVOIDING_OBSTACLE;	
 		} else {
-			this.currentMode = "goAround";
+			this.state = State.MOVING;
 		}
 
-
-		if (this.currentMode == "goAround") {
-
+		if (this.state == State.MOVING) {
 			this.setTranslationalVelocity(Velocity);
-
-			// frequently change orientation
+			
 			if ((getCounter() % 100) == 0) {
 				setRotationalVelocity(Math.PI / 2 * (0.5 - Math.random()));
 			}
 
-			Coordinate currentLocation = new Coordinate();
-			Point3d loc = new Point3d();
-			this.getCoords(loc);
-
+			Point3d loc = location();
 			for (int i = myCell.coordinatePool.length-1; i>=0; i--) {
 				if ((Math.abs((Math.abs((double)myCell.coordinatePool[i].xValue) - Math.abs(loc.x))) < 0.2 ) && 
-						(Math.abs((Math.abs((double)myCell.coordinatePool[i].yValue) - Math.abs(loc.z))) < 0.2 )) {              // close enough
-					// take photo and remove coordinate from coordinate pool
-					System.out.println("On coordinate");
+					(Math.abs((Math.abs((double)myCell.coordinatePool[i].yValue) - Math.abs(loc.z))) < 0.2 )) {
+					// take photo and remove coordinate from coordinate pool if rover is close to coordinate from coordinate pool
 					//	takePhoto();
 					myCell.removeFromCoordinatePool(myCell.coordinatePool[i]);
+					mainEnvironment.removeFromCoordinatePool(mainEnvironment.coordinatePool[i]);
 				}
 			}
-
-		} else if (this.currentMode == "onTheCellEdge") {
-			// don't move
+		} else if (this.state == State.EDGE_OF_CELL) {
 			this.Velocity=-Velocity;
 			this.setTranslationalVelocity(Velocity);		
-
 		} else {
 			this.Velocity=-Velocity;
 			this.setTranslationalVelocity(Velocity);
 		}
 	}
-
-
 
 	public void stopRover() {
 		setTranslationalVelocity(0.0);
@@ -113,6 +103,4 @@ public class RoverSimulator extends Agent {
 		picArray[numberOfImages] = cameraImage;
 		numberOfImages++;
 	}
-
-
 }
