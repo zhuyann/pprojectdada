64104b664194278147a9f4965ef7100f76b29ec4
tmv200 <t.marjanov@student.vu.nl>
2017-03-27 14:23:17 +0200

    Update RoverSimulator.java

--- RoverSimulator.java
+++ RoverSimulator.java
@@ -15,7 +15,7 @@ public class RoverSimulator extends Agent {
 	//private int maxDistance = EnvironmentSimulator.WORLD_SIZE / 2 - 1;	
 	double Velocity;
 	Vector3d initialPosition;
-	
+
 	BufferedImage[] picArray;
 	int numberOfImages;
 	BufferedImage cameraImage;
@@ -33,69 +33,75 @@ public class RoverSimulator extends Agent {
 		picArray = new BufferedImage[10];
 		numberOfImages = 0;
 	}
-	
+
 	private Point3d location() {
 		Point3d loc = new Point3d();
 		this.getCoords(loc);
 		return loc;
 	}
-	
+
 
 	public void initBehavior() {
 		setTranslationalVelocity(10.5);
 	}
-	
+
 	private boolean onTheCellEdge() {
 
 		Point3d loc = location();
-		
-		
-	 return initialPosition.getX()-1 >= loc.getX() || loc.getX() >=(initialPosition.getX() + maxDistance)
-		 || initialPosition.getZ()+1 <= loc.getZ() || loc.getZ() <=(initialPosition.getZ() - maxDistance);
+
+
+		return initialPosition.getX()-1 >= loc.getX() || loc.getX() >=(initialPosition.getX() + maxDistance)
+				|| initialPosition.getZ()+1 <= loc.getZ() || loc.getZ() <=(initialPosition.getZ() - maxDistance);
 
 	}
 
 	public void performBehavior() {
-		if (collisionDetected()) {
-		//	System.out.println("collision");
 
-			//collision avoidance strategy
 
-			
-		} else if(onTheCellEdge()) {
-			System.out.print(name);
-			System.out.println("on edge");
-			stopRover();
+		if (onTheCellEdge()) {
+			this.currentMode = "onTheCellEdge";
+		} else if (this.collisionDetected()) {
+			this.currentMode = "avoidObstacle";	
+		} else {
+			this.currentMode = "goAround";
 		}
-		
-		
-		else {
-			// progress at 0.5 m/s
-			setTranslationalVelocity(0.5);
-			if ((getCounter() % 100)==0) 
-				setRotationalVelocity(Math.PI/2 * (0.5 - Math.random()));
-			
-			// check current location and see if we are on coordinate to take photo
+
+
+		if (this.currentMode == "goAround") {
+
+			this.setTranslationalVelocity(Velocity);
+
+			// frequently change orientation
+			if ((getCounter() % 100) == 0) {
+				setRotationalVelocity(Math.PI / 2 * (0.5 - Math.random()));
+			}
+
 			Coordinate currentLocation = new Coordinate();
 			Point3d loc = new Point3d();
-	        this.getCoords(loc);
-	//		currentLocation = currentLocation.translate(loc);
-			for (int i = myCell.coordinatePool.length-1; i>=0; i--) {
-				
+			this.getCoords(loc);
 
-	//			if ((double)myCell.coordinatePool[i].xValue == loc.x && (double)myCell.coordinatePool[i].yValue == loc.y) {              == exactly on coordinate
-				if ((Math.abs((Math.abs((double)myCell.coordinatePool[i].xValue) - Math.abs(loc.x))) < 0.2 ) && (Math.abs((Math.abs((double)myCell.coordinatePool[i].yValue) - Math.abs(loc.z))) < 0.2 )) {              // close enough
+			for (int i = myCell.coordinatePool.length-1; i>=0; i--) {
+				if ((Math.abs((Math.abs((double)myCell.coordinatePool[i].xValue) - Math.abs(loc.x))) < 0.2 ) && 
+						(Math.abs((Math.abs((double)myCell.coordinatePool[i].yValue) - Math.abs(loc.z))) < 0.2 )) {              // close enough
 					// take photo and remove coordinate from coordinate pool
 					System.out.println("On coordinate");
-				//	takePhoto();
+					//	takePhoto();
 					myCell.removeFromCoordinatePool(myCell.coordinatePool[i]);
-					
 				}
 			}
-	//		System.out.print(loc.x);
-	//		System.out.println(loc.y);
+
+		} else if (this.currentMode == "onTheCellEdge") {
+			// don't move
+			this.Velocity=-Velocity;
+			this.setTranslationalVelocity(Velocity);		
+
+		} else {
+			this.Velocity=-Velocity;
+			this.setTranslationalVelocity(Velocity);
 		}
 	}
+
+
 
 	public void stopRover() {
 		setTranslationalVelocity(0.0);
