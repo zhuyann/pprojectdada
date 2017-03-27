88f269eaf3724af1bbd7f26be8b8b7d46f2c1d96
tmv200 <t.marjanov@student.vu.nl>
2017-03-27 12:33:12 +0200

    Update RoverSimulator.java

--- RoverSimulator.java
+++ RoverSimulator.java
@@ -1,10 +1,9 @@
+package rovuSystem;
 
 import java.awt.image.BufferedImage;
 
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
-
-import nl.vu.cs.softmod.examples.MultiRobot.Direction;
 
 import simbad.sim.Agent;
 import simbad.sim.CameraSensor;
@@ -12,34 +11,40 @@ import simbad.sim.RobotFactory;
 
 public class RoverSimulator extends Agent {
 	private String currentMode;
-	private int maxDistance = 4;
+	private int maxDistance = 4;                   // hardcoded in this case, we should calculate it instead
+	//private int maxDistance = EnvironmentSimulator.WORLD_SIZE / 2 - 1;	
 	double Velocity;
+	Vector3d initialPosition;
+	
 	BufferedImage[] picArray;
 	int numberOfImages;
 	BufferedImage cameraImage;
 	CameraSensor camera;
-	Vector3d initialPosition;
-	// private int maxDistance = EnvironmentSimulator.WORLD_SIZE / 2 - 1;
+	Environment myCell;
 
-	public RoverSimulator(Vector3d position, String name) {
-
-		super(position, name);
+	public RoverSimulator (Vector3d position, String name, Environment cell){   
+		super(position,name);
 		Velocity = 0.5;
 		initialPosition = position;
-		RobotFactory.addSonarBeltSensor(this, 4);
+		myCell = cell;
+		RobotFactory.addSonarBeltSensor(this, 4);  
 		camera = RobotFactory.addCameraSensor(this);
 		cameraImage = camera.createCompatibleImage();
 		picArray = new BufferedImage[10];
 		numberOfImages = 0;
-
 	}
-
+	
 	private Point3d location() {
 		Point3d loc = new Point3d();
 		this.getCoords(loc);
 		return loc;
 	}
+	
 
+	public void initBehavior() {
+		setTranslationalVelocity(10.5);
+	}
+	
 	private boolean onTheCellEdge() {
 
 		Point3d loc = location();
@@ -50,43 +55,45 @@ public class RoverSimulator extends Agent {
 
 	}
 
-	public void initBehavior() {
-		setTranslationalVelocity(Velocity);
-	}
-
 	public void performBehavior() {
-        
-		
-		if (this.collisionDetected()) {
-			this.currentMode = "avoidObstacle";
-		} else if (onTheCellEdge()) {
-			this.currentMode = "onTheCellEdge";
+		if (collisionDetected()) {
+		//	System.out.println("collision");
+
+			//collision avoidance strategy
+
 			
-		} else {
-			this.currentMode = "goAround";
+		} else if(onTheCellEdge()) {
+		//	System.out.print(name);
+		//	System.out.println("on edge");
+	
 		}
 		
-
-		if (this.currentMode == "goAround") {
+		
+		else {
+			// progress at 0.5 m/s
+			setTranslationalVelocity(0.5);
+			if ((getCounter() % 100)==0) 
+				setRotationalVelocity(Math.PI/2 * (0.5 - Math.random()));
 			
-			this.setTranslationalVelocity(Velocity);
+			// check current location and see if we are on coordinate to take photo
+			Coordinate currentLocation = new Coordinate();
+			Point3d loc = new Point3d();
+	        this.getCoords(loc);
+	//		currentLocation = currentLocation.translate(loc);
+			for (int i = myCell.coordinatePool.length-1; i>=0; i--) {
+				
 
-			// frequently change orientation
-			if ((getCounter() % 100) == 0) {
-				setRotationalVelocity(Math.PI / 2 * (0.5 - Math.random()));
+	//			if ((double)myCell.coordinatePool[i].xValue == loc.x && (double)myCell.coordinatePool[i].yValue == loc.y) {              == exactly on coordinate
+				if ((Math.abs((Math.abs((double)myCell.coordinatePool[i].xValue) - Math.abs(loc.x))) < 0.2 )) { // (Math.abs((Math.abs((double)myCell.coordinatePool[i].yValue) - Math.abs(loc.y))) < 0.7 )) {              // close enough
+					// take photo and remove coordinate from coordinate pool
+					System.out.println("On coordinate");
+				//	takePhoto();
+					myCell.removeFromCoordinatePool(myCell.coordinatePool[i]);
+					
+				}
 			}
-		} else if (this.currentMode == "onTheCellEdge") {
-			// don't move
-			this.Velocity=-Velocity;
-			this.setTranslationalVelocity(Velocity);
-			// rotate only until obstacle is not there
-			
-
-		} else {
-			// don't move
-			this.setTranslationalVelocity(0);
-			// rotate only until obstacle is not there
-			setRotationalVelocity(Math.PI / 2);
+	//		System.out.print(loc.x);
+	//		System.out.println(loc.y);
 		}
 	}
 
@@ -100,5 +107,6 @@ public class RoverSimulator extends Agent {
 		picArray[numberOfImages] = cameraImage;
 		numberOfImages++;
 	}
+
 
 }
