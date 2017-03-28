cdc6c8c530a7721b51d42bfb2721aebed05b1188
tmv200 <t.marjanov@student.vu.nl>
2017-03-28 15:10:07 +0200

    Update Coordinate.java

--- Coordinate.java
+++ Coordinate.java
@@ -1,30 +1,16 @@
 package rovuSystem;
 
-import javax.vecmath.Point3d;
-
 public class Coordinate {
 
 	int xValue;
-	
 	int yValue;
-	
-	Coordinate() {
-		xValue = 0;
-		yValue = 0;
-		
-	}
 	
 	Coordinate(int x, int y){
 		this.xValue = x;
 		this.yValue = y;	
 	}
-	Boolean isEqual(Coordinate coordinate){
-	return this.xValue==coordinate.xValue&&this.yValue==coordinate.yValue;
-		
-	}
 	
-	Coordinate translate(Point3d coordinate3d) {
-		Coordinate coordinate = new Coordinate ((int)(coordinate3d.x), (int)(coordinate3d.y));
-		return coordinate;
+	public Boolean isEqual(Coordinate coordinate) {
+		return this.xValue==coordinate.xValue&&this.yValue==coordinate.yValue;
 	}
 }
