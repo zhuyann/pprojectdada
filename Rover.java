1fc491079e87c7688a4af0f37e3dae1685513d2c
tmv200 <t.marjanov@student.vu.nl>
2017-04-01 16:50:24 +0200

    Update Rover.java

--- Rover.java
+++ Rover.java
@@ -1,57 +1,46 @@
 package rovuSystem;
 
-
-
 import java.awt.image.BufferedImage;
 
-
 public class Rover extends Observer{
-	
+
 	String name;
 	Environment myCell;
 	State state;
-	
+
 	BufferedImage[] pictures;
 	int numberOfPictures;
-	
+
 	RoverSimulator roverSim;
-	
-	
+
+
 	public Rover(RoverSimulator roverSimulator, Environment cell, State still,CentralStation centralStation) {
-	      this.subject = centralStation;
-	      this.subject.attach(this);
+		this.subject = centralStation;
+		this.subject.attach(this);
 		roverSim = roverSimulator;
 		state = still;
 	}
-	
-	 @Override
-	void stopRovers() {
-		if(state.equals("stop")){
-			roverSim.stopRover();
-		}
-		
-		//implement instructions from the Central Station
-	}
-	
-	
-	void stop() {
-		roverSim.stopRover();
-		state = State.STILL;
-	}
-	
+
 	void avoidCollision() {
 		roverSim.performBehavior();
 	}
-	
+
 	void communicateWithTheCS() {
 		//send feedback to Central Station
 	}
-	
-	void takeAndSendPhotos() {
+
+	void sendPhotos() {
 		roverSim.takePhoto();
 		pictures = roverSim.picArray;
 		numberOfPictures = roverSim.picArray.length;
 	}
-	
+
+	void stopRovers() {
+		state = State.STILL;
+		roverSim.stop = true;
+		roverSim.performBehavior();
+
+	}
+
 
 }
