9ae65c540634b3e4e59dbe16985ae11f97ff1f6c
tmv200 <t.marjanov@student.vu.nl>
2017-04-04 23:37:21 +0200

    Update CentralStation.java

--- CentralStation.java
+++ CentralStation.java
@@ -1,32 +1,29 @@
 package rovuSystem;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.imageio.ImageIO;
 import javax.vecmath.Vector3d;
 import simbad.sim.EnvironmentDescription;
 
 public class CentralStation extends Subject{
-	
+
 	int timer = 30;
 	double currentPercentageVisited = 0;
-	
+
 	BufferedImage[] pictureArray;
 	int lengthOfPictureArray;
 	Environment mainEnvironment;
 	static EnvironmentDescription environmentSim;
-	
+
 	Rover rover1;
 	Rover rover2;
 	Rover rover3;
 	Rover rover4;
-	
+
 	private static CentralStation instance = new CentralStation();
-	
+
 	private CentralStation() {
 		pictureArray = new BufferedImage[4000];
 		lengthOfPictureArray = 0;
@@ -34,23 +31,22 @@ public class CentralStation extends Subject{
 
 	}
 
-	// for singleton implementation
 	public static CentralStation getInstance(EnvironmentSimulator environment) {
 		environmentSim = environment;
 		return instance;
 	}
-	
+
 	void intializeMission() {	
 		mainEnvironment = new Environment(10,10, new Coordinate(-5,-5));
 		int lengthOfEachCell = mainEnvironment.length / 2;
 		int widthOfEachCell = mainEnvironment.width / 2;
-				
+
 		Environment cell1 = new Environment(lengthOfEachCell, widthOfEachCell, new Coordinate(0,0));
 		Environment cell2 = new Environment(lengthOfEachCell, widthOfEachCell, new Coordinate(0,-widthOfEachCell));
 		Environment cell3 = new Environment(lengthOfEachCell, widthOfEachCell, new Coordinate(-lengthOfEachCell,-widthOfEachCell));
 		Environment cell4 = new Environment(lengthOfEachCell, widthOfEachCell, new Coordinate(-lengthOfEachCell,0));
-		
-        rover1 = new Rover(new RoverSimulator(new Vector3d(-4, 0, -1), "rover1", cell1, mainEnvironment), cell1,
+
+		rover1 = new Rover(new RoverSimulator(new Vector3d(-4, 0, -1), "rover1", cell1, mainEnvironment), cell1,
 				State.STILL,instance);
 		rover2 = new Rover(new RoverSimulator(new Vector3d(1, 0, -1), "rover2", cell2, mainEnvironment), cell2,
 				State.STILL,instance);
@@ -59,68 +55,64 @@ public class CentralStation extends Subject{
 		rover4 = new Rover(new RoverSimulator(new Vector3d(1, 0, 4), "rover4", cell4, mainEnvironment), cell4,
 				State.STILL,instance);
 		environmentSim.add(rover1.roverSim);
-		
+
 		environmentSim.add(rover2.roverSim);
-		
+
 		environmentSim.add(rover3.roverSim);
-		
+
 		environmentSim.add(rover4.roverSim);
-		
+
 		cell1.defineCoordinates();
 		cell2.defineCoordinates();
 		cell3.defineCoordinates();
 		cell4.defineCoordinates();
 		mainEnvironment.defineCoordinates();
+		//	cell4.lampOn = true;
 	}
-	
+
 	public void runMission () {
-		
+
 		boolean missionIncomplete = true;
-		
+
 		while (missionIncomplete) {
 			if (mainEnvironment.percentageVisited() >= 70.0 ) {
 				System.out.println("The rovers covered more than 70%, but will continue running for a while.");
 				missionIncomplete = false;
 			}
 		}
-		
+
 		currentPercentageVisited = mainEnvironment.percentageVisited();
 		Timer t = new Timer();
 		t.schedule(new TimerTask() {
-	        @Override
-	        public void run() {
-	        	if(currentPercentageVisited == 100.0) {
-	        		finishMission();
-	        	} else if (timer != 0 && currentPercentageVisited == mainEnvironment.percentageVisited()) {
-	        		timer -= 1;
-	        		try {
+			@Override
+			public void run() {
+				if(currentPercentageVisited == 100.0) {
+					finishMission();
+				} else if (timer != 0 && currentPercentageVisited == mainEnvironment.percentageVisited()) {
+					timer -= 1;
+					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
 					}
-	        		run();
-	        	} else
-	        	if (timer != 0 && currentPercentageVisited != mainEnvironment.percentageVisited()) {
-	        		currentPercentageVisited = mainEnvironment.percentageVisited();
-	        		timer = 30;
-	        		try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-					}
-	        		run();
-	        	} else {
-	        		finishMission();
-	        	} 
-	        }
-	    }, 1000);
+					run();
+				} else
+					if (timer != 0 && currentPercentageVisited != mainEnvironment.percentageVisited()) {
+						currentPercentageVisited = mainEnvironment.percentageVisited();
+						timer = 30;
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+						}
+						run();
+					} else {
+						finishMission();
+					} 
+			}
+		}, 1000);
 	}
-	
 
 	void finishMission(){
-		System.out.printf("Mission complete! \nThe rovers visited %.3f %% of the environment. \n ", currentPercentageVisited);
-		rover1.stopRovers();
-		rover2.stopRovers();
-		rover3.stopRovers();
-		rover4.stopRovers();
+		setState("stop");		
 		savePictures();
 		try {
 			Thread.sleep(10000);
@@ -128,12 +120,12 @@ public class CentralStation extends Subject{
 		}
 		System.exit(0);
 	}
-	
+
 	void savePictures() {
 		System.out.println("Pictures have been saved.");
 	}
-	
+
 	void handleError() {
-		
+
 	}
 }
